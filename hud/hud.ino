79da759260a69439cd4fa2d2d58aa04e32c44997
Gediminas Kaknevičius <buvomano@yahoo.com>
2018-05-20 18:35:08 +0300

    Coordinates, distance, heading, alt, pitch

--- hud/hud.ino
+++ hud/hud.ino
@@ -3,13 +3,25 @@
 #include <Adafruit_HMC5883_U.h>
 #include <TVout.h>
 #include <TVoutfonts/fontALL.h>
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
 
 //Variables:
 int FOV = 30; // field of view
-int tgtHdg = 20; // target heading
+// Pressent position:
+double presX = 23.318922;
+double presY = 55.925182;
+int presElev = 200; // altitude in meters
+// Target position:
+double tgtX = 23.317962;
+double tgtY = 55.925614;
+int tgtElev = 100; // target elevation in meters
 
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
 
 TVout TV;
 //unsigned char x,y;
@@ -29,7 +41,7 @@ void setup() {
   if(!mag.begin())
   {
     /* There was a problem detecting the HMC5883 ... check your connections */
-    //Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
+    Serial.println("Ooops, no HMC5883 detected ... Check your wiring!");
     while(1);
   }
   
@@ -73,7 +85,7 @@ void loop() {
     heading -= 2*PI;
    
   // Convert radians to degrees for readability.
-  // int compass = heading * 180/M_PI; UNCOMENT//////////////////////////
+   //int compass = heading * 180/M_PI; // UNCOMENT//////////////////////////
   
   //COMENT:
   int sensorHdg = analogRead(A0);
@@ -85,26 +97,11 @@ void loop() {
   }*/
 
   //Serial.print("Compass: "); Serial.println(compass);
-  /*
-  if (compass < 10)
-  {
-    Serial.print("Compass: "); Serial.print("00"); Serial.print(compass);
-  }
-  else if (compass < 100)
-  {
-   Serial.print("Compass: "); Serial.print('0'); Serial.print(compass);
-  }
-  else
-  {
-    Serial.print("Compass: "); Serial.println(compass);
-  }
-*/
-  //Serial.print("Compass: "); Serial.println(compass);
   
   // Calculate offset from center:
   int remainder = compass % 5;
   int offset = 5 - remainder;
-   offset = map(remainder, 0, 5, 0, spaces); // offset changed to pixels
+  offset = map(remainder, 0, 5, 0, spaces); // offset changed to pixels
   //Serial.print("Offset: "); Serial.println(offset);
 
   //Drawing stuff:
@@ -169,15 +166,44 @@ void loop() {
       {
         TV.print(x, y, output);
       }
-      //TV.print(x, y, output);
       //Serial.print("Output: "); Serial.println(output);
     }
     
   }
 
   //Calculate and draw target circle:
+  //Calculate target heading and distance:
+  
+  //Change altitude with pot for testing:
+  presElev = analogRead(A0);
+  presElev = map(presElev, 0, 1023, 0, 2000);
+  
+  //double presX = gps.location.lng(); UNCOMENT FOR GPS!!!!!!!!!!!!
+  //double presY = gps.location.lat();
+  double groundDistance =
+    TinyGPSPlus::distanceBetween(
+      presY,
+      presX,
+      tgtY, 
+      tgtX);
+  
+  float alt = presElev - tgtElev;
+  double distance = sqrt(sq(groundDistance/1000)+sq(alt/1000))*1000L; // corrected for altitude
+  
+  double tgtHdg =
+    TinyGPSPlus::courseTo(
+      presY,
+      presX,
+      tgtY, 
+      tgtX);
+      
+  Serial.print("Altitude                               : "); Serial.println(alt);
+  Serial.print("Air distance: "); Serial.println(distance);
+  //Serial.print("Target heading: "); Serial.println(tgtHdg);
+  Serial.print("Ground distance: "); Serial.println(groundDistance);
+  
+
   //Calculating delta:
-  //int delta = tgtHdg - compass;
   int delta = tgtHdg - compass;
   if (delta >= 180)
   {
@@ -189,8 +215,11 @@ void loop() {
   }
 
   //Calculating alfa:
-  int alfa = ver/2;
-/*
+  //int alfa = ver/2;
+  float alfa = acos(alt/distance);
+  alfa = alfa * 180/M_PI - 90;
+  Serial.print("Alfa: "); Serial.println(alfa);
+  
   //Drawing circle:
   if (delta < 15 && delta > -15)
   {
@@ -198,23 +227,12 @@ void loop() {
   }
   if (delta > 15)
   {
-    draw_arrow(90);
+    draw_arrow(0);
   }
   if (delta < -15)
   {
-    draw_arrow(270);
+    draw_arrow(180);
   }
-*/
-  draw_arrow(compass); 
-  
-  /*
-  int sensorHdg=analogRead(A0);
-  int x = map(sensorHdg, 0, 1023, 0, 127);
-  TV.clear_screen();
-  int y=0;
-  TV.draw_line(64,63,x,y,1);
-  //TV.delay_frame(1);
-  */
   
   
   TV.delay_frame(1);
